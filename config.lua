4a34bafd37b23e106d5af88df9e3f114c051f9cc
MasiBall <76224425+MasiBall@users.noreply.github.com>
2021-12-20 00:44:03 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,13 +1,13 @@
 Config = {}
 
-Config.UseOldESX = true
+Config.UseOldESX = true -- Set to true if you don't have imports.lua file in your es_extended
 Config.DiscordLog = true
-Config.Key = 74
+Config.Key = 74 -- Key to beg
 Config.ShouldWaitBetweenBegging = true
 Config.Cooldown = 60 -- Duration of cooldown (In seconds)
 
 Config.MaxDistance = 3 -- Maximum distance to target ped
-Config.AnimationDuration = 5 -- How long is the animation (In seconds)
+Config.AnimationDuration = 11 -- How long is the animation (In seconds)
 
-Config.MinMoney = 100
-Config.MaxMoney = 500
+Config.MinMoney = 50
+Config.MaxMoney = 350
