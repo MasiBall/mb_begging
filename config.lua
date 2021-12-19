25f45cac29289731887ba8c1d530fc02dba8ed75
MasiBall <76224425+MasiBall@users.noreply.github.com>
2021-12-19 14:10:44 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,12 @@
 Config = {}
-Config.Locale = 'en'
 
 Config.UseOldESX = true
+Config.DiscordLog = true
 Config.ShouldWaitBetweenBegging = true
 Config.Cooldown = 60 -- Duration of cooldown (In seconds)
 
 Config.MaxDistance = 3 -- Maximum distance to target ped
-Config.AnimationDuration = 10 -- How long is the animation (In seconds)
+Config.AnimationDuration = 5 -- How long is the animation (In seconds)
 
 Config.MinMoney = 100
 Config.MaxMoney = 500
