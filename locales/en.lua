09ab0118666aca87723952950966a6503b0bcb27
MasiBall <76224425+MasiBall@users.noreply.github.com>
2021-12-20 00:20:12 +0200

    Delete locales directory

--- locales/en.lua
+++
@@ -1,5 +0,0 @@
-Locales['en'] = {
-    ['target_too_far'] = "Go closer so you don't have to shout",
-    ['begging'] = "Pls give money!",
-    ['begging_completed'] = "The person was nice and gave you ~g~$%s~s~",
-}
--- locales/fi.lua
+++
@@ -1,5 +0,0 @@
-Locales['fi'] = {
-    ['target_too_far'] = "Mene lähemmäksi niin ei tarvitse huutaa",
-    ['begging'] = "Anna rahaa pls!",
-    ['begging_completed'] = "Sait ~g~$%s~s~ rahaa",
-}
