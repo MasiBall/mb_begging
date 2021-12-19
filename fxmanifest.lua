0df526fc04c115ea7b1273f9ea34ae2a655aaf02
MasiBall <76224425+MasiBall@users.noreply.github.com>
2021-12-20 00:48:16 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version 'cerulean'
 games { 'gta5' }
-author 'Base code by c0deina, edited by Masi' -- https://github.com/c0deina/esx_robnpc
+author 'Base code by c0deina, modified by MasiBall' -- https://github.com/c0deina/esx_robnpc
 
 shared_scripts {
     '@es_extended/imports.lua',
